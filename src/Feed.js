f8d6dabd88aef78ea322e94851052ed163e9779f
medabde <medabde9@gmail.com>
2021-01-15 19:34:23 +0100

    updating the post component and adding an iframe

--- src/Feed.js
+++ src/Feed.js
@@ -9,7 +9,20 @@ function Feed() {
         <div className="feed">
             <StoryReel/>
             <MessageSender />
-            <Post/>
+            <Post
+                profilePic=''
+                message='hello'
+                timestamp="this is a timestamp"
+                username="moha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuCC19Y7E34QpbIn5q8f7JONuIjaVNbwHqMg&usqp=CAU"
+            />
+            <Post
+                profilePic=''
+                message='hello'
+                timestamp="this is a timestamp"
+                username="moha"
+                image=""
+            />
         </div>
     )
 }
--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,47 @@
+import { Avatar } from '@material-ui/core'
+import { AccountCircle, ChatBubbleOutline, ExpandMoreOutlined, NearMe, ThumbUp } from '@material-ui/icons'
 import React from 'react'
 import './Post.css'
 
-function Post() {
+function Post({profilePic,image,username,timestamp,message}) {
     return (
         <div className="post">
-            <h1>Test Post</h1>
+            <div className="post__top">
+                <Avatar src={profilePic}
+                className="post__avatar"/>
+                <div className="post__topInfo">
+                    <h3>{username}</h3>
+                    <p>timestamp</p>
+                </div>
+                 
+            </div>
+            <div className="post__bottom">
+                <p>{message}</p>
+            </div>
+            <div className="post__image">
+                <img src={image} alt="" srcset=""/>
+            </div>
+
+            <div className="post__options">
+                <div className="post__option">
+                    <ThumbUp />
+                    <p>Like</p>
+                </div>
+                <div className="post__option">
+                    <ChatBubbleOutline />
+                    <p>Comment</p>
+                </div>
+                <div className="post__option">
+                    <NearMe />
+                    <p>Share</p>
+                </div>
+                <div className="post__option">
+                    <AccountCircle/>
+                    <ExpandMoreOutlined/>
+                </div>
+
+
+            </div>
         </div>
     )
 }
